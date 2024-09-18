015dcbd980f97108e04466d124a618d49945d9a3
Javier Minto Arze <diegominto2@gmail.com>
2024-09-18 11:01:42 -0400

    Corrección de errores con imágenes

--- app.py
+++ app.py
@@ -7,10 +7,18 @@ from models.category import Category
 from controllers.category_controller import category_blueprint
 from controllers.product_controller import product_blueprint
 from controllers.user_controller import user_blueprint
+from flask import send_from_directory
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Define la carpeta de archivos estáticos
+app.config['UPLOAD_FOLDER'] = 'static/imagenes'
+
+@app.route('/static/<path:filename>')
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 # Asociar la base de datos con la app
 db.init_app(app)
@@ -36,11 +44,20 @@ def crear_producto_form():
     return render_template('crear_producto.html', categorias=categorias)
 
 # app.py
-@app.route('/productos/<int:id>', methods=['GET'])
-def mostrar_producto(id):
-    producto = Producto.query.get(id)
-    if producto is None:
-        return jsonify({"message": "Producto no encontrado"}), 404
+@app.route('/producto/<int:id>')
+def producto_detalle(id):
+    producto = {
+        'id': id,
+        'nombre': 'Laptop blanca',
+        'descripcion': 'Laptop blanca con tecla color azul',
+        'precio': 100.0,
+        'stock': 5,
+        'marca': 'HP',
+        'modelo': 'Pavilion Gaming',
+        'especificaciones': '8GB RAM, 256 GB almacenamiento',
+        'imagen_url': '/static/imagenes/laptop1.png',
+        'categoria': {'name': 'Computadoras'}
+    }
     return render_template('producto_detalle.html', producto=producto)
 
 
